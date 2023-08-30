f551987ab8ee25c85bbfcaa3a4d16e841d4091fd
Sarah Mota <143041192+SarahMota@users.noreply.github.com>
2023-08-30 13:57:17 -0300

    renomear

--- src/app/estruturassequenciais/Exercicio1.java
+++ src/app/estruturassequenciais/Exercicio1.java
@@ -5,7 +5,7 @@ para aprender*/
 
 package app.estruturassequenciais;
 
-public class exercício1 {
+public class Exercicio1 {
     public static void main(String [] args) {
         System.out.println("É preciso fazer todos os exercícios para aprender!");
 
--- src/app/estruturassequenciais/Exercicio2.java
+++ src/app/estruturassequenciais/Exercicio2.java
@@ -5,7 +5,7 @@ package app.estruturassequenciais;
 
 import java.util.Scanner;
 
-public class exercício2 {
+public class Exercicio2 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int a,b;
