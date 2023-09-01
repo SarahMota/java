015335f71f524618effe9381a5aec2f5f66507d5
Sarah Mota <143041192+SarahMota@users.noreply.github.com>
2023-09-01 19:08:16 -0300

    new

--- src/matrizes/licao2.java
+++ src/matrizes/licao2.java
@@ -3,10 +3,10 @@ package matrizes;
 public class licao2 {
     public static void main(String [] args) {
         int[][] matriz = new int[2][2];
-         // Modificar os elementos nas posições especificadas
-        matriz[1][1] = 10; // Elemento na posição (1, 1)
-        matriz[1][0] = 20; // Elemento na posição (1, 0)
-        matriz[0][1] = 30; // Elemento na posição (0, 1)
+        
+        matriz[1][1] = 10; 
+        matriz[1][0] = 20; 
+        matriz[0][1] = 30; 
         
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
