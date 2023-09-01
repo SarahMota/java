73efee40f3b8b8f95b7d11618c1d93031bf4e43f
Sarah Mota <143041192+SarahMota@users.noreply.github.com>
2023-09-01 20:47:11 -0300

    new

--- src/matriz/licao1.java
+++ src/matriz/licao1.java
@@ -3,15 +3,15 @@ package matriz;
 public class licao1 {
     public static void main(String [] args) {
         int[][] matriz = {
-            {3, 7},
-            {1, 4},
-            {6, 8}
+            {3, 5, 7},
+            {1, 7, 4},
+            {6, 3, 8}
         };
 
        
-        int elemento22 = matriz[1][1]; 
-        int elemento21 = matriz[1][0]; 
-        int elemento12 = matriz[0][1]; 
+        int elemento22 = matriz[0][0]; 
+        int elemento21 = matriz[1][1]; 
+        int elemento12 = matriz[0][2]; 
 
        
         System.out.println("Elemento na segunda linha e segunda coluna: " + elemento22);
