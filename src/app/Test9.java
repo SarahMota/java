e042bbac441571734d6462a17bdd6a2e39154f96
Sarah Mota <143041192+SarahMota@users.noreply.github.com>
2023-09-09 13:32:58 -0300

    news

---
+++ src/app/Test9.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Test9 {
+    public static void main(String[] args) {
+      int valor = 123465;
+      valor=valor%10;
+      valor=valor*2;
+      valor=valor+100;
+      valor=valor%10;
+      int voce=10;
+      
+      System.out.println("Valor: "+valor);
+    }
+    
+}
--- src/estruturassequencias/Exercicio1.java
+++ src/estruturassequencias/Exercicio1.java
@@ -6,7 +6,7 @@ para aprender*/
 package estruturassequencias;
 
 public class Exercicio1 {
-    public static void main(String [] args) {
+    public static void main(String [] args) { 
         System.out.println("É preciso fazer todos os exercícios para aprender!");
 
     }
